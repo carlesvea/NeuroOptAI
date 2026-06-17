6122742401488edf1dc2ae9e9a7a7b30a67e35b6
Carles X. Vea <carles.vea@gmail.com>
2026-06-17 16:33:04 +0200

    Integrate gradient clipping into training controller

--- neurooptai/controllers/training_controller.py
+++ neurooptai/controllers/training_controller.py
@@ -1,22 +1,28 @@
 from neurooptai.sensors.loss_sensor import LossSensor
+from neurooptai.sensors.gradient_norm_sensor import GradientNormSensor
 from neurooptai.diagnostics.overfitting_detector import OverfittingDetector
 from neurooptai.actions.early_stopping_action import EarlyStoppingAction
+from neurooptai.actions.gradient_clipping_action import GradientClippingAction
 
 
 class TrainingController:
-    """
-    Primer cervell de NeuroOptAI:
-    registra losses, detecta overfitting i recomana accions.
-    """
-
-    def __init__(self):
+    def __init__(self, gradient_threshold=1.0):
         self.loss_sensor = LossSensor()
+        self.gradient_norm_sensor = GradientNormSensor()
         self.overfitting_detector = OverfittingDetector()
         self.early_stopping_action = EarlyStoppingAction()
+        self.gradient_clipping_action = GradientClippingAction(max_norm=gradient_threshold)
+        self.gradient_threshold = gradient_threshold
 
-    def log_epoch(self, train_loss, validation_loss):
+    def log_epoch(self, train_loss, validation_loss, gradient_norm=None):
         self.loss_sensor.log_train_loss(train_loss)
         self.loss_sensor.log_validation_loss(validation_loss)
+
+        if gradient_norm is not None:
+            self.gradient_norm_sensor.log_gradient_norm(gradient_norm)
+
+            if self.gradient_norm_sensor.is_too_high(self.gradient_threshold):
+                return self.gradient_clipping_action.execute()
 
         is_overfitting = self.overfitting_detector.detect(
             self.loss_sensor.train_losses,
