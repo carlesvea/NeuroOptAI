cea5d887dbae520854a51808272dbc1c1d4c110e
Carles X. Vea <carles.vea@gmail.com>
2026-06-17 16:53:23 +0200

    Integrate stagnation and learning rate reduction

--- neurooptai/controllers/training_controller.py
+++ neurooptai/controllers/training_controller.py
@@ -1,36 +1,48 @@
 from neurooptai.sensors.loss_sensor import LossSensor
 from neurooptai.sensors.gradient_norm_sensor import GradientNormSensor
+from neurooptai.sensors.learning_rate_sensor import LearningRateSensor
 from neurooptai.diagnostics.overfitting_detector import OverfittingDetector
+from neurooptai.diagnostics.stagnation_detector import StagnationDetector
 from neurooptai.actions.early_stopping_action import EarlyStoppingAction
 from neurooptai.actions.gradient_clipping_action import GradientClippingAction
+from neurooptai.actions.reduce_learning_rate_action import ReduceLearningRateAction
 
 
 class TrainingController:
-    def __init__(self, gradient_threshold=1.0):
+    def __init__(self, gradient_threshold=1.0, stagnation_patience=3):
         self.loss_sensor = LossSensor()
         self.gradient_norm_sensor = GradientNormSensor()
+        self.learning_rate_sensor = LearningRateSensor()
+
         self.overfitting_detector = OverfittingDetector()
+        self.stagnation_detector = StagnationDetector(patience=stagnation_patience)
+
         self.early_stopping_action = EarlyStoppingAction()
         self.gradient_clipping_action = GradientClippingAction(max_norm=gradient_threshold)
+        self.reduce_lr_action = ReduceLearningRateAction(factor=0.5)
+
         self.gradient_threshold = gradient_threshold
 
-    def log_epoch(self, train_loss, validation_loss, gradient_norm=None):
+    def log_epoch(self, train_loss, validation_loss, gradient_norm=None, learning_rate=None):
         self.loss_sensor.log_train_loss(train_loss)
         self.loss_sensor.log_validation_loss(validation_loss)
 
+        if learning_rate is not None:
+            self.learning_rate_sensor.log_learning_rate(learning_rate)
+
         if gradient_norm is not None:
             self.gradient_norm_sensor.log_gradient_norm(gradient_norm)
-
             if self.gradient_norm_sensor.is_too_high(self.gradient_threshold):
                 return self.gradient_clipping_action.execute()
 
-        is_overfitting = self.overfitting_detector.detect(
+        if self.overfitting_detector.detect(
             self.loss_sensor.train_losses,
             self.loss_sensor.validation_losses
-        )
-
-        if is_overfitting:
+        ):
             return self.early_stopping_action.execute()
+
+        if self.stagnation_detector.detect(self.loss_sensor.validation_losses):
+            return self.reduce_lr_action.execute(current_lr=learning_rate)
 
         return {
             "action": "continue_training",
