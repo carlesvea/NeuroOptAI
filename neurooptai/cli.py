de77ca993e08b3e8199923aab3858d07e3dab086
Carles X. Vea <carles.vea@gmail.com>
2026-06-18 14:17:21 +0200

    Add CLI version command

--- neurooptai/cli.py
+++ neurooptai/cli.py
@@ -1,6 +1,8 @@
 import argparse
 from neurooptai.controllers.training_controller import TrainingController
 
+VERSION = "0.1.0"
+
 
 def run_demo():
     controller = TrainingController(gradient_threshold=1.0, stagnation_patience=2)
@@ -17,20 +19,14 @@ def run_demo():
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description="NeuroOptAI command line interface"
-    )
-
-    parser.add_argument(
-        "command",
-        choices=["demo"],
-        help="Command to execute"
-    )
-
+    parser = argparse.ArgumentParser(description="NeuroOptAI command line interface")
+    parser.add_argument("command", choices=["demo", "version"], help="Command to execute")
     args = parser.parse_args()
 
     if args.command == "demo":
         run_demo()
+    elif args.command == "version":
+        print(f"NeuroOptAI {VERSION}")
 
 
 if __name__ == "__main__":
