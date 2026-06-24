3afecbd2352c27104066ccb18f167d08660cca52
Carles X. Vea <carles.vea@gmail.com>
2026-06-24 16:19:55 +0200

    Use Universal HALO decision in demo

--- experiments/demo_universal_halo.py
+++ experiments/demo_universal_halo.py
@@ -2,8 +2,8 @@ from neurooptai import UniversalHALO
 
 halo = UniversalHALO()
 
-print("simple:", halo.should_intervene(3, 10))
-print("score:", halo.score(3, 10))
-print("class:", halo.classify(3, 10))
-print("expected:", halo.expected_avoided_cost(0.8, 100))
-print("probabilistic:", halo.should_intervene_probabilistic(20, 0.8, 100))
+print(halo.decision(
+    intervention_cost=3,
+    avoided_cost=10,
+    probability_of_failure=0.8,
+))
